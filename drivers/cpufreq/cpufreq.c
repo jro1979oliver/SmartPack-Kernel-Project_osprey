df852f1f09ca932758e12e7c2af25218958ef57b
Viresh Kumar <viresh.kumar@linaro.org>
2012-10-31 01:28:21 +0100

    cpufreq: Make sure target freq is within limits
    __cpufreq_driver_target() must not pass target frequency beyond the
    limits of current policy.
    
    Today most of cpufreq platform drivers are doing this check in their
    target routines. Why not move it to __cpufreq_driver_target()?
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1794,11 +1794,12 @@ int __cpufreq_driver_target(struct cpufreq_policy *policy,
 	if (target_freq < policy->min)
 		target_freq = policy->min;
 
-	if (target_freq == policy->cur)
-		return 0;
-
 	pr_debug("target for CPU %u: %u kHz, relation %u, requested %u kHz\n",
 			policy->cpu, target_freq, relation, old_target_freq);
+
+
+	if (target_freq == policy->cur)
+		return 0;
 
 	if (cpufreq_driver->target)
 		retval = cpufreq_driver->target(policy, target_freq, relation);
